91498c3e0b8acb54d25f857dfa4526e60ce64087
JayQkew <rayjebb@gmail.com>
2024-02-28 19:51:04 +0200

    camera functionality

--- Partnership/Assets/_Scripts/CameraLogic.cs
+++ Partnership/Assets/_Scripts/CameraLogic.cs
@@ -6,33 +6,68 @@ using UnityEngine;
 public class CameraLogic : MonoBehaviour
 {
     public CinemachineVirtualCamera virtualCamera;
-    public GameObject player;
+    public GameObject mainShip;
 
     public float zoomSpeed;
+    public float moveDelta;
+
+    public GameObject[] focusedShips = new GameObject[0];
+    public LayerMask shipLayer;
+    public Vector2 colliderArea;
+
 
     private void Update()
     {
-        FollowPlayer();
-        //CameraZoom();
+        FleetCollider();
+        FollowFleet();
     }
 
-    public void FollowPlayer()
+    public void FollowFleet()
     {
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+        if (focusedShips.Length <= 0)
+        {
+            transform.position = new Vector3(mainShip.transform.position.x, mainShip.transform.position.y, transform.position.z);
+        }
+        else
+        {
+            Vector2 totalPos = Vector2.zero;
+
+
+            foreach (GameObject go in focusedShips)
+            {
+                totalPos += (Vector2)go.transform.position;
+            }
+
+            Vector2 center = totalPos / focusedShips.Length;
+
+            transform.position = Vector3.Lerp(transform.position, new Vector3(center.x, center.y, transform.position.z), moveDelta * Time.deltaTime);
+        }
     }
 
-    //public void CameraZoom()
-    //{
-    //    if (SpaceShipeLogic.Instance.speed >= 10)
-    //    {
+    public void ZoomToVelocity()
+    {
 
-    //        float targetSize = SpaceShipeLogic.Instance.speed * 2;
+    }
 
-    //        virtualCamera.m_Lens.OrthographicSize = Mathf.Lerp(virtualCamera.m_Lens.OrthographicSize, targetSize, zoomSpeed * Time.deltaTime);
-    //    }
-    //    else
-    //    {
-    //        virtualCamera.m_Lens.OrthographicSize = Mathf.Lerp(virtualCamera.m_Lens.OrthographicSize, 20, zoomSpeed* 2 * Time.deltaTime);
-    //    }
-    //}
+    public void FleetCollider()
+    {
+        RaycastHit2D[] hit = Physics2D.BoxCastAll(transform.position, colliderArea, 0, Vector3.zero, 0, shipLayer);
+        List<GameObject> sur_ships = new List<GameObject>();
+
+        for (int i = 0; i < hit.Length; i++)
+        {
+            if (hit[i].transform.gameObject != gameObject)
+            {
+                sur_ships.Add(hit[i].transform.gameObject);
+            }
+        }
+
+        focusedShips = sur_ships.ToArray();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, colliderArea);
+    }
+
 }
